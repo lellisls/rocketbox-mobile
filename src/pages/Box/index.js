7c9f7f86953987300a908acc734f16854b06ca9c
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-19 21:54:09 -0300

    Open File

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -11,18 +11,78 @@ import pt from 'date-fns/locale/pt';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import ImagePicker from 'react-native-image-picker';
+import RNFS from 'react-native-fs';
+import FileViewer from 'react-native-file-viewer';
+
 export default class Box extends Component {
   state = {box: {}};
 
   async componentDidMount() {
     const box = await AsyncStorage.getItem('@RocketBox:box');
-    const response = await api.get(`boxes/${box}`);
-    console.log(box);
-    this.setState({box: response.data});
+    try {
+      const response = await api.get(`boxes/${box}`);
+      console.log(box);
+      this.setState({box: response.data});
+    } catch (err) {
+      await AsyncStorage.setItem('@RocketBox:box', '');
+      this.props.navigation.navigate('Main');
+    }
   }
 
+  handleUpload = () => {
+    ImagePicker.launchImageLibrary({}, async upload => {
+      if (upload.error) {
+        console.error('ImagePicker error: ', upload.error);
+      } else if (upload.didCancel) {
+        console.log('Canceled by user');
+      } else {
+        console.log(upload);
+
+        const data = new FormData();
+
+        const [prefix, suffix] = upload.fileName.split('.');
+        const ext = suffix.toLowerCase() === 'heic' ? 'jpg' : suffix;
+
+        data.append('file', {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`,
+        });
+
+        api.post(`boxes/${this.state.box._id}/files`, data);
+      }
+    });
+  };
+
+  openFile = async file => {
+    try {
+      const filePath = `${RNFS.DocumentDirectoryPath}/${file.title}`;
+      const options = {
+        fromUrl: file.url,
+        toFile: filePath,
+      };
+
+      console.log('options', options);
+
+      const res = await RNFS.downloadFile(options).promise;
+
+      console.log('res', res);
+
+      console.log(filePath);
+
+      await FileViewer.open(filePath);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   renderItem = ({item}) => (
-    <TouchableOpacity onPress={() => {}} style={styles.file}>
+    <TouchableOpacity
+      onPress={() => {
+        this.openFile(item);
+      }}
+      style={styles.file}>
       <View style={styles.fileInfo}>
         <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
         <Text style={styles.fileTitle}>{item.title}</Text>
@@ -47,6 +107,10 @@ export default class Box extends Component {
           ItemSeparatorComponent={() => <View style={styles.separator} />}
           renderItem={this.renderItem}
         />
+
+        <TouchableOpacity style={styles.fab} onPress={this.handleUpload}>
+          <Icon name="cloud-upload" size={24} color="#FFF" />
+        </TouchableOpacity>
       </View>
     );
   }
